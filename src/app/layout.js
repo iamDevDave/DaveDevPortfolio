282fe31ba63c70a11c83d5f26864907434bd87da
Dave Dev <draxerdrv@gmail.com>
2024-03-19 23:02:00 +0530

    add seo

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,11 +7,15 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Dave Dev",
   description: "The Dev animated portfolio Home page",
+  
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <meta name="google-site-verification" content="t6SW8jHr2hR4PeVW8PsYI-ckUc6W-7vpSs_jSNgVC8Q" />
+      </head>
       <body className={inter.className}>
         <TransitionProvider>{children}</TransitionProvider>
       </body>
